8cf187fb1f3baf2621c787c4e1687c55692b39df
cernanb <cernanb@gmail.com>
2019-11-14 17:59:15 -0800

    Create event model

--- app/models/pin_release.rb
+++ app/models/pin_release.rb
@@ -1,4 +1,5 @@
 class PinRelease < ApplicationRecord
     belongs_to :pin
     belongs_to :company, optional: true
+    belongs_to :event, optional: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_184346) do
+ActiveRecord::Schema.define(version: 2019_11_15_015535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,12 @@ ActiveRecord::Schema.define(version: 2019_11_09_184346) do
   end
 
   create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
